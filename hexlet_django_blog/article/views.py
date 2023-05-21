ba5f9e796e1febeae66e1b3ecf0532df50779b58
Vladislav25 <vladkhleborodov@gmail.com>
2023-05-21 20:36:04 +0300

    add views classes

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from hexlet_django_blog.article.apps import ArticleConfig
+from django.views import View
 
+
+class IndexView(View):
+
+    app_name = ArticleConfig.name
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'articles/index.html', context={'name': IndexView.app_name})
 
 def index(request):
     app_name = ArticleConfig.name
--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,5 +1,19 @@
 # *hexlet_django_blog/views.py*
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+class HomePageView(TemplateView):
+
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['who'] = 'Vlad'
+        return context
+    
+    def index(self, request):
+        return render(request, HomePageView.template_name, context=self.get_context_data)
+
 
 def index(request):
     return render(request, 'index.html', context={
