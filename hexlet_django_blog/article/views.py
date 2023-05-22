d6e011e70211e1df5e135a9ee5c0c3076709f717
Vladislav25 <vladkhleborodov@gmail.com>
2023-05-22 18:03:46 +0300

    add new article route

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from hexlet_django_blog.article.apps import ArticleConfig
 from django.views import View
+from django.urls import reverse
 
 
 class IndexView(View):
@@ -8,8 +9,9 @@ class IndexView(View):
     app_name = ArticleConfig.name
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'articles/index.html', context={'name': IndexView.app_name})
+        return redirect(reverse('python_article', kwargs={'tags': 'python', 'article_id': 42}))
 
-def index(request):
+
+def article(request, tags: str, article_id: int):
     app_name = ArticleConfig.name
-    return render(request, 'articles/index.html', context={'name': app_name})
+    return render(request, 'articles/index.html', context={'name': app_name, 'tags': tags, 'article_id': article_id})
